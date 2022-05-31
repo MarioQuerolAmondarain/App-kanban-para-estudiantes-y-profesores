f40497685d3962c9a92ea704c9a43aec863aa24a
ElisaGP <elisagobiernop@gmail.com>
2022-05-31 20:30:01 +0200

    Arrancar unico Servicio

--- proyectoapi2/src/main/java/com/proyectoDAM/rest/proyectoapi2/Proyectoapi2Application.java
+++ proyectoapi2/src/main/java/com/proyectoDAM/rest/proyectoapi2/Proyectoapi2Application.java
@@ -1,17 +1,17 @@
 package com.proyectoDAM.rest.proyectoapi2;
 
 import java.sql.SQLException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import Users.*;
-
+import com.proyectoDAM.rest.Usuarios.*;
 
 @SpringBootApplication
 public class Proyectoapi2Application {
 
 	public static void main(String[] args) throws SQLException {
 		SpringApplication.run(Proyectoapi2Application.class, args);
-		
+		SpringApplication.run(ProyectoApiUsuarios.class, args);
 		
 	}
 	
